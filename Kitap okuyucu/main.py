9ace90e2e0d34a4f18034f082e6ca60d24c859ba
MuhammedEfe-Yildiz <112964932+MuhammedEfe-Yildiz@users.noreply.github.com>
2022-09-06 19:37:13 +0300

    book-reader
    PDF reader app...

---
+++ Kitap okuyucu/main.py
@@ -0,0 +1,17 @@
+
+import pyttsx3
+import PyPDF2
+
+hikaye = open("machinelearning.pdf","rb")
+pdfOkuyucu = PyPDF2.PdfFileReader(hikaye)
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+
+engine.setProperty('voices', voices[0].id)
+
+for sayfa_numarasi in range(0, pdfOkuyucu.numPages):
+	sayfa = pdfOkuyucu.getPage(sayfa_numarasi)
+	yazi = sayfa.extractText()
+	engine.say(yazi)
+	engine.runAndWait()
